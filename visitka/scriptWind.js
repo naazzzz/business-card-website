d6a65bf8f3036c63b862fe151ddc708e5a6c2b6c
Artyom <artem_berezhnov@mail.ru>
2021-08-05 20:19:20 +0300

    instal commit

---
+++ visitka/scriptWind.js
@@ -0,0 +1,20 @@
+	var b = document.getElementById('overlay');
+function swab() {
+	b.style.visibility = 'visible';
+	b.style.opacity = '1';
+	b.style.transition = 'all 0.7s ease-out 0s';
+}
+function swa2b() {
+	b.style.visibility = 'hidden';
+	b.style.opacity = '0';
+}
+var d = document.getElementById('overlay-1');
+function swad() {
+	d.style.visibility = 'visible';
+	d.style.opacity = '1';
+	d.style.transition = 'all 0.7s ease-out 0s';
+}
+function swa2d() {
+	d.style.visibility = 'hidden';
+	d.style.opacity = '0';
+}
